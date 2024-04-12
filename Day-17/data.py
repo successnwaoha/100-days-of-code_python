fd767a9ce368e4f01abf54d74e2518a20f959314
unknown <nwaohasuccess2306@gmail.com>
2024-04-12 20:31:59 +0100

    Day 17

---
+++ Day-17/data.py
@@ -0,0 +1,72 @@
+question_data = [
+    {
+        "category":"Science: Computers",
+        "question":"The NVidia GTX 1080 gets its name because it can only render at a 1920x1080 screen resolution.",
+        "correct_answer":"False",
+        "incorrect_answers":["True"]
+        
+    },
+    {
+        "category":"Science: Computers",
+        "question":"The programming language &quot;Python&quot; is based off a modified version of &quot;JavaScript&quot;.",
+        "correct_answer":"False",
+        "incorrect_answers":["True"]
+        
+    },
+    {
+        "category":"Science: Computers",
+        "question":"The Windows ME operating system was released in the year 2000.",
+        "correct_answer":"True",
+        "incorrect_answers":["False"]
+                             
+    },
+    {
+        "category":"Science: Computers",
+        "question":"RAM stands for Random Access Memory.",
+        "correct_answer":"True",
+        "incorrect_answers":["False"]
+        
+    },
+    {
+        "category":"Science: Computers",
+        "question":"Linux was first created as an alternative to Windows XP.",
+        "correct_answer":"False",
+        "incorrect_answers":["True"]
+     
+    },
+    {
+        "category":"Science: Computers",
+        "question":"In most programming languages, the operator ++ is equivalent to the statement &quot;+= 1&quot;.",
+        "correct_answer":"True",
+        "incorrect_answers":["False"]
+        
+    },
+    {
+        "category":"Science: Computers",
+        "question":"Linus Torvalds created Linux and Git.",
+        "correct_answer":"True",
+        "incorrect_answers":["False"]
+        
+    },
+    {
+        "category":"Science: Computers",
+        "question":"The logo for Snapchat is a Bell.",
+        "correct_answer":"False",
+        "incorrect_answers":["True"]
+     
+    },
+    {
+        "category":"Science: Computers",
+        "question":"The Windows 7 operating system has six main editions.",
+        "correct_answer":"True",
+        "incorrect_answers":["False"]
+     
+    },
+    {
+        "category":"Science: Computers",
+        "question":"&quot;HTML&quot; stands for Hypertext Markup Language.",
+        "correct_answer":"True",
+        "incorrect_answers":["False"]
+        
+    }
+]
